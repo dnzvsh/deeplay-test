fc52176580eb1e85316c541854ac8524f52b40cc
dnzvsh <zuevdanil@gmail.com>
2022-06-14 12:53:06 +0700

    Add editEmployeeInfoById() to the List class

--- src/list.ts
+++ src/list.ts
@@ -2,8 +2,8 @@ import {Employee} from './employee';
 
 class List {
   employeeArray: Array<Employee>;
-  constructor() {
-    this.employeeArray = [];
+  constructor(employeeArray: Array<Employee>) {
+    this.employeeArray = employeeArray;
   }
 
   addEmployee(employee: Employee): void {
@@ -17,6 +17,13 @@ class List {
     delete this.employeeArray[this.employeeArray.indexOf(employee)];
   }
 
+  editEmployeeInfoById(id: number, key: string, newValue: string): void {
+    let employee: Employee = this.findEmployeeById(id);
+    if (employee) {
+      (employee as any)[key] = newValue;
+    }
+  }
+
   promoteEmployeeById(
       id: number, newPosition: string, newAdditionalInfo: object): void {
     let employee: Employee = this.findEmployeeById(id);
@@ -27,7 +34,22 @@ class List {
   }
 
 
-  findEmployee() {}
+  findEmployees(value: string, key?: string): List {
+    let foundEmployeesList: List = new List([]);
+    if (key) {
+      for (const employee of this.employeeArray) {
+        for (const prop in employee) {
+          // это работает только внутри "верхних уровней класса"
+          // надо сделать разверту доп.информации как обычных полей класса
+          // либо придумать что-то еще
+          if (prop === key && (employee as any)[prop] == value) {
+            foundEmployeesList.addEmployee(employee);
+          }
+        }
+      }
+    }
+    return foundEmployeesList;
+  }
 
   deleteEmployeeById(id: number): void {
     delete this.employeeArray[id];
