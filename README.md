6fa07c599f0dfaef276dfa06a29c713a57b65b53
dnzvsh <zuevdanil@gmail.com>
2022-06-14 22:14:23 +0700

    Finish write a lib for app

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # deeplay-test
 Test task for Deeplay Intership.
 
+# WARNING!
+На данный момент проект представляет из себя библиотеку для приложения, а не само приложение.
+Возникло это из-за невнимательности при прочтении задания.
+
+Есть надежда, что это изменится до того, как это кто-то увидит. Если же нет, то частично работу можно оценить, посмотрев тесты: `/tests/employees.test.ts`
+
+
 # Tools
-- TypeScript
-- Node JS
+- TypeScript v4.7.3
+- Node JS v16.13.2
 
 # Usage
-## Run
+## Tests
+```
+npm run test
+```
+
+## ~~Run~~
 ```bash
 tsc
 node dist/app.js
-```
-
-## Tests
-```
-tsc
-npm run test
 ```
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,4 @@
-import {Employee} from './employee.js'
-import {List} from './list.js'
+import {EmployeesList, Position} from './employeeList'
+import {Controller, Director, Manager, Person, Worker} from './employees';
 
-const john = new Employee(
-    'John Grey', 67, 'male', 'Consultant', {subdivision: 'Relationship'});
-
-const joanne = new Employee(
-    'J.K. Rowling', 45, 'female', 'Writer', {subdivision: 'Fantasy'});
-
-const roma = new Employee(
-    'Roman Yaroshev', 23, 'male', 'Student',
-    {subdivision: 'SibSUTIS', department: 'EVM'});
-
-const factory = new List([]);
-
-factory.addEmployee(john);
-factory.addEmployee(joanne);
-factory.addEmployee(roma);
-
-const foundEmployeesList = factory.findEmployees('male', 'sex');
-
-console.log('\nOnly male employees:\n')
-foundEmployeesList.employeesList();
-
-
-console.log('\nEmployee List:\n')
-factory.employeesList();
-factory.deleteEmployee(roma);
-
-const johnId = factory.getEmployeeId(john);
-
-factory.promoteEmployeeById(
-    johnId, 'Programmer', {company: 'Yandex', department: 'Backend'});
-
-console.log('\nList after delete employee:\n')
-factory.employeesList();
+console.log('Work in progress, please check later')
