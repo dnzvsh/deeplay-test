f7e67c65e2adf21a20530e075ef3f268c6e74615
dnzvsh <zuevdanil@gmail.com>
2022-06-14 13:58:36 +0700

    Add test for director + delete ID from class

--- tests/employees.test.ts
+++ tests/employees.test.ts
@@ -1,14 +1,25 @@
-import {Manager} from '../src/employees';
+import {Director, Manager} from '../src/employees';
 
 const managerMac = new Manager('John Grey', 67, 'male', 'MacDonalds');
+
+const directorAmazon = new Director('Jeffrey Preston', 68, 'male', 'Amazon');
 
 test('Manager getInfo', () => {
   const managerInfo: string = managerMac.getInfo();
 
-  expect(managerInfo).toBe(`id: 0
-fullName: John Grey
+  expect(managerInfo).toBe(`fullName: John Grey
 age: 67
 sex: male
 position: manager
 subdivision: MacDonalds`);
+});
+
+test('Director getInfo', () => {
+  const directorInfo: string = directorAmazon.getInfo();
+
+  expect(directorInfo).toBe(`fullName: Jeffrey Preston
+age: 68
+sex: male
+position: director
+company: Amazon`);
 });
