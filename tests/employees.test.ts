4bc54f3c1cf1449e1b6b46d08f0f62faf7ada90e
dnzvsh <zuevdanil@gmail.com>
2022-06-14 14:10:51 +0700

    Add test for class Worker

--- tests/employees.test.ts
+++ tests/employees.test.ts
@@ -1,13 +1,15 @@
-import {Director, Manager} from '../src/employees';
+import {Director, Manager, Worker} from '../src/employees';
 
 const managerMac = new Manager('John Grey', 67, 'male', 'MacDonalds');
 
 const directorAmazon = new Director('Jeffrey Preston', 68, 'male', 'Amazon');
 
+const worker = new Worker('Alice Smith', 25, 'female', 'Chief Keef');
+
 test('Manager getInfo', () => {
   const managerInfo: string = managerMac.getInfo();
 
-  expect(managerInfo).toBe(`fullName: John Grey
+  expect(managerInfo).toBe(`full name: John Grey
 age: 67
 sex: male
 position: manager
@@ -17,9 +19,19 @@ subdivision: MacDonalds`);
 test('Director getInfo', () => {
   const directorInfo: string = directorAmazon.getInfo();
 
-  expect(directorInfo).toBe(`fullName: Jeffrey Preston
+  expect(directorInfo).toBe(`full name: Jeffrey Preston
 age: 68
 sex: male
 position: director
 company: Amazon`);
+});
+
+test('Worker getInfo', () => {
+  const workerInfo: string = worker.getInfo();
+
+  expect(workerInfo).toBe(`full name: Alice Smith
+age: 25
+sex: female
+position: worker
+manager's full name: Chief Keef`);
 });
